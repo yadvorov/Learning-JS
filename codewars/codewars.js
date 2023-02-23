cae92a4052fdc66170aa0ab900e46e269987cc13
Yaroslav Dvorov <serg.ya.u@gmail.com>
2023-02-23 11:41:46 +0300

    codewars/codewars.js

---
+++ codewars/codewars.js
@@ -0,0 +1,30 @@
+////требуется написать функцию которая при четном числе умножает на 8, при нечетном умножаеи на 9
+
+// function simpleMultiplication (number) {
+//     if (number%2) { //если есть остаток от деления значит число нечетное 
+//         return number * 9;
+//     } else {
+//         return number * 8;
+//     }
+// } 
+// console.log(simpleMultiplication(10))
+
+
+// //функция умножающая количество учеников на количество бумаг, в случае если отрицательное количетсво выдает 0
+// function papework(n, m) {
+//     if (n < 0 || m < 0) {
+//         return 0;
+//     } else {
+//         return n * m;
+//     }
+
+// }
+// console.log (papework (8, 6))
+
+//фунцкия которая возвращает приветствие с именем:
+
+function greet(name) {
+    return `Hello, ${name} do you know that you are pidor?`
+}
+
+console.log(greet("Dima"));
