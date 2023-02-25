ef1890771c3505a9eec73eeca457a64ea63751dd
Yaroslav Dvorov <serg.ya.u@gmail.com>
2023-02-25 18:38:44 +0300

    code.mu

---
+++ code.mu/codemu.js
@@ -0,0 +1,172 @@
+// //Практика на поиск ошибок:
+
+// let num1 = 1;
+// let num2 = 2;
+// console.log("сумма:" + (num1 + num2))
+
+// let a = 1;
+// let b = 2;
+// let c = 0;
+// console.log(a + b + c);
+
+// let num3 = '123';
+// let sum = +num3[0] + +num3[1] + +num3[2];
+// console.log(sum);
+
+// let num= "123";
+// console.log(num[0]);
+
+// let f = 0;
+// console.log(++f);
+
+// let num0 = "123";
+// console.log(num0.length);
+
+// let x = 24 * 60 * 60;
+// console.log(x);
+
+// let num4 = 12345678;
+// let str = String(num4);
+// console.log(str.length);
+
+// let num11 = 129;
+// let str1 = String(num11);
+// console.log(str1[2]);
+
+// let num7 = 11111111111;
+// let str2 = String(num7);
+// console.log(str2.length);
+
+// let num8 = 128;
+// let str3 = String(num8);
+// console.log(str3[2]);
+
+// let a1 = '123';
+// let b1 = '456';
+// let s1 = +Number(a1) + +String(b1);
+// console.log(s1);
+
+
+// let aaa = 1;
+// let bbb = 2;
+// let ccc = 3;
+
+// console.log(aaa + bbb + ccc);
+
+//Массивы
+
+// let arr = [1, 2, 3, 4, 5]
+// alert(arr);
+// console.log(arr)
+
+// let arr1 = ['dog', 'cat', 'bird']
+// alert(arr1);
+// console.log(arr1)
+
+
+// let arr = [1, 2, 3]
+
+// // console.log(arr[0]);
+// // console.log(arr[1]);
+// // console.log(arr[2]);
+
+// // alert(arr[0]);
+// // alert(arr[1]);
+// // alert(arr[2]);
+
+// alert(arr[0]+arr[1]+arr[2])
+
+// let arr = [1, 2, 4, 5]
+
+
+// console.log(arr.length)
+// console.log(arr[arr.length-1]);
+
+
+
+// let arr = [4,5,6]
+// arr[0] += 3;
+// arr[1] += 3;
+// arr[2] += 3;
+// console.log(arr)
+
+// let arr=[];
+// arr[0] = 1;
+// arr[1] = 2;
+// arr[2] = 3
+// arr[3] = 4;
+// arr[4] = 5;
+// console.log(arr)
+
+// let arr = [1, 2, 3];
+// arr[3] = 4;
+// arr[4] = 5;
+// console.log(arr)
+
+// let arr = ['a', 'b', 'c'];
+
+// arr[0] = 1;
+// arr[1] = 2;
+// arr[2] = 3;
+// arr.push(4, 5)
+// console.log(arr)
+
+// let arr = [1, 2 , 3, 4 ,5]
+// let key = 2;
+// let key1 = 3;
+// console.log(arr[key]  + arr[key1])
+
+// let arr = ['a', 'b' ,'c' , 'd' , 'e']
+// delete arr[0];
+// delete arr[4];
+// console.log(arr.length)
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr[4]);
+
+// let arr= [1, 2 , 3 , 4 ,5 ];
+// console.log(arr[0] + arr[1] + arr[2] + arr[3] + arr[4])
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr.length);
+
+
+// let arr = ['a', 'b' ,'c', 'd']
+
+// let result = '';
+// for (let i = 0; i < arr.length; i++ ) {
+//     if (i === 0) {
+//         result = result + arr[i].toUpperCase();
+//     } else { 
+//         result= result + arr[i];
+//     }
+// }
+
+// console.log(result)
+
+//  array[i] = [1 ,2 ,3] - обращение по индексу
+//         i = 0   1  2  - индекс
+
+// function sanek(string, number) {
+
+//     if (number <= 0 || number > string.length || typeof string != "string") {
+//         return "Uncorrect!"
+//     } 
+    
+//     let arr = string.split("");
+//     for (let i = 0; i < arr.length; i++) {
+//     if (i === number - 1) {
+//         arr[i] = arr[i].toUpperCase();
+//     } 
+//     } 
+//     return arr.join("")
+// }   
+// console.log(sanek("sanek", 2));
+
+
+
+
+
+
+
+
