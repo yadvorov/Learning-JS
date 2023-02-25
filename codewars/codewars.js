f3b1282bf7ec16fd77593c02bb9e76cb2e93aa31
Yaroslav Dvorov <serg.ya.u@gmail.com>
2023-02-25 12:25:08 +0300

    codewars.js

--- codewars/codewars.js
+++ codewars/codewars.js
@@ -53,7 +53,38 @@
 
 // console.log(warnTheSheep(arr));
 
+// // Square(n) Sum
 
 
 
+// function squareSum(numbers){
+//     let sum = 0;
+//     for (let i = 0; i < numbers.length; i++) {
+//         sum += numbers[i] **2
+//     }
+//     return sum
+// }
+// console.log(squareSum(3, 3))
 
+
+// Grasshopper - Grade book
+
+function getGrade (s1, s2, s3) {
+    let score = ((s1 + s2 + s3)/3)
+    if (score >= 90 && score <= 100) {
+        return "A"
+    }
+    if (score >= 80 && score <= 90) {
+        return "B"
+    }
+    if (score >= 70 && score <= 80) {
+        return "C"
+    } 
+    if (score >= 60 && score <= 70) {
+        return "D"
+    }
+    else {
+        return "F"
+    }
+}
+console.log(getGrade (90, 50, 100))
