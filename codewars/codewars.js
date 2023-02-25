a13585c3682d522e51a3ebdf1af1171fc3f06187
Yaroslav Dvorov <serg.ya.u@gmail.com>
2023-02-25 18:32:52 +0300

    code.mu

--- codewars/codewars.js
+++ codewars/codewars.js
@@ -69,22 +69,29 @@
 
 // Grasshopper - Grade book
 
-function getGrade (s1, s2, s3) {
-    let score = ((s1 + s2 + s3)/3)
-    if (score >= 90 && score <= 100) {
-        return "A"
-    }
-    if (score >= 80 && score <= 90) {
-        return "B"
-    }
-    if (score >= 70 && score <= 80) {
-        return "C"
-    } 
-    if (score >= 60 && score <= 70) {
-        return "D"
-    }
-    else {
-        return "F"
-    }
-}
-console.log(getGrade (90, 50, 100))
+// function getGrade (s1, s2, s3) {
+//     let score = ((s1 + s2 + s3)/3)
+//     if (score >= 90 && score <= 100) {
+//         return "A"
+//     }
+//     if (score >= 80 && score <= 90) {
+//         return "B"
+//     }
+//     if (score >= 70 && score <= 80) {
+//         return "C"
+//     } 
+//     if (score >= 60 && score <= 70) {
+//         return "D"
+//     }
+//     else {
+//         return "F"
+//     }
+// }
+// console.log(getGrade (90, 50, 100))
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr[arr.length - 1]);
+
+
+
+
